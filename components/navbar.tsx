0d380beecb822f52afaddb2ddea6cfc3f948e67b
alexanderhernandez17 <alexander.hdez.2001gmail.com>
2024-10-26 16:13:10 -0500

    updates

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -76,7 +76,7 @@ export const Navbar = () => {
                 href="./team"
                 size="lg"
               >
-                {item.label}
+                {}
               </Link>
             </NavbarMenuItem>
           ))}
